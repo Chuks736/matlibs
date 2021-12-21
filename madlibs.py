eafd2c21d53eae5c0af5b0c5de05f41c457dbedc
Chukwuma igboanusi <94975413+Chuks736@users.noreply.github.com>
2021-12-21 06:43:04 +0000

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb')
+famous_person = input('Famous person: ')
+matlib = f'Computer programing is so {adj}! it makes me so excited all the time because i love ' \
+         f'to {verb1}. Stay hydrated and  {verb2} like you are {famous_person}!'
+
+print(matlib)
